472ca7c3511e68720ab294842ced088673712a77
andreipakemp <andrei.pak.emp@gmail.com>
2021-04-28 16:04:57 -0400

    reset local

--- .virtualenvs/myvirtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/andreipakportfolio/.virtualenvs/myvirtualenv/bin/python3.8
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- portfolio/Quizzer/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .views import *
-from .viewsQuestions import *
-from .viewsQuiz import *
-
-def main(request):
-    return render(request, 'main.html', {'title': "Quizzer Main"})
